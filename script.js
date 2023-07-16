db29cce5237de19c1ea2d017a57fd7d9c5289160
Avinaash984 <131980080+Avinaash984@users.noreply.github.com>
2023-07-17 00:53:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,41 @@
-// Card data
-const cards = [
-    { id: 1, img: 'image1.jpg' },
-    { id: 2, img: 'image2.jpg' },
-    { id: 3, img: 'image3.jpg' },
-    { id: 4, img: 'image4.jpg' },
-    { id: 5, img: 'image1.jpg' },
-    { id: 6, img: 'image2.jpg' },
-    { id: 7, img: 'image3.jpg' },
-    { id: 8, img: 'image4.jpg' }
-  ];
-  
-  let flippedCards = [];
-  let score = 0;
-  let numClicks = 0;
-  
-  // Shuffle cards
-  const shuffledCards = shuffle(cards.concat(cards));
-  
-  // Create game board
-  const gameBoard = document.getElementById('game-board');
-  shuffledCards.forEach(card => {
-    const cardElement = document.createElement('div');
-    cardElement.classList.add('card');
-    cardElement.setAttribute('data-id', card.id);
-    cardElement.addEventListener('click', flipCard);
-    const imgElement = document.createElement('img');
-    imgElement.src = card.img;
-    cardElement.appendChild(imgElement);
-    gameBoard.appendChild(cardElement);
-  });
-  
-  // Card click event handler
-  function flipCard() {
-    if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
-      this.classList.add('flipped');
-      flippedCards.push(this);
-      numClicks++;
-  
-      if (flippedCards.length === 2) {
-        checkForMatch();
-      }
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(Card => Card.addEventListener('click',flipcard));
+
+
+let cardsflipped = false;
+let cardone;
+let cardtwo;
+
+function flipcard(){
+    this.classList.add('flip');
+
+    if(!cardsflipped){
+        cardsflipped = true;
+        cardone = this;
     }
-  }
-  
-  // Check if flipped cards match
-  function checkForMatch() {
-    const card1 = flippedCards[0];
-    const card2 = flippedCards[1];
-  
-    const id1 = card1.getAttribute('data-id');
-    const id2 = card2.getAttribute('data-id');
-  
-    if (id1 === id2) {
-      score++;
-      document.getElementById('score').textContent = `Score: ${score}`;
-      flippedCards = [];
-      if (score === cards.length) {
-        setTimeout(() => {
-          alert('Congratulations! You won the game.');
-        }, 500);
-      }
-    } else {
-      setTimeout(() => {
-        card1.classList.remove('flipped');
-        card2.classList.remove('flipped');
-        flippedCards = [];
-      }, 1000);
+    else{
+        cardsflipped = false;
+        cardtwo = this;
     }
-  }
-  
-  // Shuffle an array
-  function shuffle(array) {
-    let currentIndex = array.length;
-    let temporaryValue, randomIndex;
-  
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+
+    if(cardone.dataset.framework === cardtwo.dataset.framework) {
+            firstCard.removeEventListener('click',flipcard);
+            secondCard.removeEventListener('click',flipcard);
     }
-  
-    return array;
-  }
-  
+    else{
+     setTimeout(() => {
+        cardone.classList.remove('flip');
+        cardtwo.classList.remove('flip');
+        },1500);
+    }
+}
+
+
+
+(function shuffle(){
+    cards.forEach(Card =>{
+        let shuffling = Math.floor(Math.random()*16);
+        Card.style.order = shuffling;
+    })
+})
