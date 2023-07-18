2c36ddd7dfac76271b560b88dc0287d4d0daadf7
Avinaash984 <131980080+Avinaash984@users.noreply.github.com>
2023-07-18 12:12:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ cards.forEach(Card => Card.addEventListener('click',flipcard));
 let cardsflipped = false;
 let cardone;
 let cardtwo;
+let scores=0;
+let time=0;
+let timer=null;
 
 function flipcard(){
     this.classList.add('flip');
@@ -22,6 +25,8 @@ function flipcard(){
     if(cardone.dataset.framework === cardtwo.dataset.framework) {
             firstCard.removeEventListener('click',flipcard);
             secondCard.removeEventListener('click',flipcard);
+            scores++;
+            document.getElementById("score").textContent = `SCORE:${scores}`;
     }
     else{
      setTimeout(() => {
@@ -33,9 +38,45 @@ function flipcard(){
 
 
 
-(function shuffle(){
+function shuffle(){
     cards.forEach(Card =>{
         let shuffling = Math.floor(Math.random()*16);
         Card.style.order = shuffling;
-    })
-})
+    });
+}
+function Timer(){
+    timer = setInterval(() =>{
+       (time++) ;
+        document.getElementById("time").textContent = `TIME: ${time}`;
+        
+    if(time > 60){
+        clearTimeout(timer);
+        windows.alert("timer ended!");
+    }
+    }, 1000);
+
+    // if(time > 60){
+    //     clearInterval(timer);
+    //     windows.alert("timer ended!");
+    // }
+}
+
+document.getElementById("restart").onclick= () =>{
+    scores = 0;
+    time = 0;
+    shuffle();
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click',flipcard);
+    });
+}
+let clickCount = document.getElementById("clicks");
+let count = 0;
+cards.forEach(card => card.addEventListener('click',clickCount));
+
+function clickCounts(){
+    count++;
+    clickCount.textContent = `CLICKS: ${count}`
+};
+shuffle()
+Timer()
